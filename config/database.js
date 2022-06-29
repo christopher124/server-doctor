75dafc3ac7d0eac35ae6299ec05998feb3c0caeb
Christopher <urri_@hotmail.com>
2022-06-29 09:20:25 -0500

    Modificaciones generales

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'server-doctor'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'hachiko42CHRIS42'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "mysql",
+        host: env("DATABASE_HOST", "35.226.32.237"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "server-doctor"),
+        username: env("DATABASE_USERNAME", "admin"),
+        password: env("DATABASE_PASSWORD", "hachiko42CHRIS42"),
+        ssl: env.bool("DATABASE_SSL", false),
       },
-      options: {}
+      options: {},
     },
   },
 });

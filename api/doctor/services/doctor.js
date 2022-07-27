ad1e37162d65c19a3254c247970a82e6b9e2bade
Christopher <urri_@hotmail.com>
2022-07-26 22:28:00 -0500

    Cambios generales

--- api/doctor/services/doctor.js
+++ api/doctor/services/doctor.js
@@ -1,8 +1,18 @@
-'use strict';
+let sendSms = function () {
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const myNum = process.env.MYNUM;
+  const twilioNum = process.env.TWILIONUM;
+  const client = require("twilio")(accountSid, authToken);
+  client.messages
+    .create({
+      body: "Hola Administrador, alguien acaba de Registrar un Doctor",
+      from: twilioNum, //the phone number provided by Twillio
+      to: myNum, // your own phone number
+    })
+    .then((message) => console.log(message.sid));
+};
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
- * to customize this service
- */
-
-module.exports = {};
+module.exports = {
+  sendSms,
+};

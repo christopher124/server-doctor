e8c5230c9c39361c18dd15befedd4430f21d2509
Christopher <urri_@hotmail.com>
2022-08-08 19:10:13 -0500

    Cambios generales en api

--- api/quotes/controllers/quotes.js
+++ api/quotes/controllers/quotes.js
@@ -1,8 +1,14 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+module.exports = {
+  async create(ctx) {
+    strapi.services.sms.sendSms();
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.quotes.create(data, { files });
+    } else {
+      entity = await strapi.services.quotes.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.quotes });
+  },
+};
--- api/quotes/services/quotes.js
+++ api/quotes/services/quotes.js
@@ -1,8 +1,18 @@
-'use strict';
+let sendSms = function () {
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const myNum = process.env.MYNUM;
+  const twilioNum = process.env.TWILIONUM;
+  const client = require("twilio")(accountSid, authToken);
+  client.messages
+    .create({
+      body: "hola Doctor, usted tiene una cita agendada, para el dia 12/08/2022 5:00 PM",
+      from: twilioNum, //the phone number provided by Twillio
+      to: myNum, // your own phone number
+    })
+    .then((message) => console.log(message.sid));
+};
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
- * to customize this service
- */
-
-module.exports = {};
+module.exports = {
+  sendSms,
+};
